6a4536a5461e2db128a4e8aabacc90d9d72a1646
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-17 07:12:19 -0700

    adding routes to server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const supabase = require('./config/supabaseClient')
+const authRoutes = require('./routes/authRoutes')
 
 dotenv.config()
 
@@ -29,6 +30,7 @@ app.get('/test-db', async (requestAnimationFrame, res) => {
         })
     }
 })
+app.use('/api/auth', authRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
