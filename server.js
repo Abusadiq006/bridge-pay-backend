a750c344ce0defb63daa500a2d7a1037157622c2
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-17 04:54:14 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const supabase = require('./config/supabaseClient')
+
+dotenv.config()
+
+const app = express()
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+//Health Check Route
+app.get('/test-db', async (requestAnimationFrame, res) => {
+    try {
+        const { data, error } = await supabase.from('profiles').select('*').limit(1)
+
+        if (error) throw error
+
+        res.status(200).json({
+            message: "Backend is connected to Supabase!",
+            data: data
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Connection failed",
+            error: error.message
+        })
+    }
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`)
+})
