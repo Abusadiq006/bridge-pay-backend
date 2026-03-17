4156ac00fe2fe54bc3f3d0f4c35c6eebbf0a0785
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-17 06:49:05 -0700

    exports

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,9 @@
 const supabase = require('../config/supabaseClient')
 
+exports.registerUser = async (req, res) => {
+    const { email, password, full_name } = req.body
+
+    
+}
+
 exports.register
