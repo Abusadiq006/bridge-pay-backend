c2993209e3df722940a8b9a05d0a3a1345f0cf19
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-17 06:54:17 -0700

    implementing try and if

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,16 @@ const supabase = require('../config/supabaseClient')
 exports.registerUser = async (req, res) => {
     const { email, password, full_name } = req.body
 
-    
+    try {
+        const { data: authData, error: authError } = await supabase.auth.signUp({
+            email,
+            password,
+        })
+
+        if (authError) throw authError
+
+        
+    }
 }
 
 exports.register
