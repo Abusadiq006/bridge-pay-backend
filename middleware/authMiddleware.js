c74068c2264547c3440eefc595725719a4ea741e
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-24 07:54:54 -0700

    implementing middleware token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,4 +2,12 @@ const supabase = require('../config/supabaseClient')
 
 const protect = async (req, res, next) => {
     let token
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1]
+
+            
+        }
+    }
 }
