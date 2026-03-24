949ef1c37f34b237560b62650499cdf20d7f7f0e
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-24 08:58:47 -0700

    implementing finduserid

--- models/Account.js
+++ models/Account.js
@@ -1,1 +1,12 @@
 const supabase = require('../config/supabaseClient')
+
+const Account = {
+    findByUserId: async (userId) => {
+        const { data, error } = await supabase
+        .from('accounts')
+        .select('*')
+        .eq('user_id', userId)
+        .single()
+        return { data, error }
+    }
+}
