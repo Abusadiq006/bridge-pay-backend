70299f043a49a435c25188cd4c80be6d5392115c
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-24 08:42:54 -0700

    Routenow protected

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,3 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const { protect } = require('../middleware/authMiddleware')
+
+router.get('/dashboard-data', protect, (req, res) => {
+    res.json({
+        message: `Welcome to your BridgePay dashboard, User ID: ${req.user.id}`,
+        balance: 5000.00
+    })
+})
+
+module.exports = router
