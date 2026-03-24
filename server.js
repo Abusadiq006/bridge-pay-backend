2cda6fea5781f60b0d937fec778e7d16a837b6c2
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-24 08:47:27 -0700

    added userRoute to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const dotenv = require('dotenv')
 const supabase = require('./config/supabaseClient')
 const authRoutes = require('./routes/authRoutes')
+const userRoutes = require('./routes/userRoutes')
 
 dotenv.config()
 
@@ -11,6 +12,7 @@ const app = express()
 // Middleware
 app.use(cors())
 app.use(express.json())
+app.use('/api/user', userRoutes)
 
 //Health Check Route
 app.get('/test-db', async (requestAnimationFrame, res) => {
