137476474f889d9a72707cbe10b591595480e8b9
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-24 09:27:36 -0700

    initiating user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,5 +3,20 @@ const supabase = require('../config/supabaseClient')
 exports.getUserProfile = async (req, res) => {
     try {
         const userId = req.user.id
+
+        const { data: profile, error: profileError } = await supabase
+        .from('profiles')
+        .selesct(`
+            full_name,
+            email,
+            role,
+            accounts (
+                account_number,
+                balance,
+                currency
+            )
+        `)
+        .eq('id', userId)
+        .single()
     }
 }
