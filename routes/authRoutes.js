4ca41bc527c5d89a709ae6d3b8ff8ba3793d49a6
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-26 07:17:51 -0700

    implementing user profile

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
+const { auth } = require('../config/supabaseClient')
 
 // Route for Registration
 router.post('/register', authController.registerUser)
 
 // Route for Login
 router.post('/login', authController.loginUser)
+
+// Route for Profile
+router.get('/profile', async )
 
 module.exports = router 
