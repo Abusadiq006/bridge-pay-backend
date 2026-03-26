964ebfc6a43d8a9578b02c57e55feeb2cf370b87
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-26 07:27:33 -0700

    if error

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,6 +12,10 @@ router.post('/login', authController.loginUser)
 // Route for Profile
 router.get('/profile', async (req, res) => {
     const token = req.headers.authorization?.split(' ')[1]
+
+    if (!token) return res.status(401).json({
+        error: 'No token provided'
+    })
 } )
 
 module.exports = router 
