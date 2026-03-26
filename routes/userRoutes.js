846b59d241d872ca27cc9aa6cfb116a93608adc0
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-26 03:53:04 -0700

    cont.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const { getUserProfile } = require('../controllers/userController')
 const { protect } = require('../middleware/authMiddleware')
 
 router.get('/dashboard-data', protect, (req, res) => {
