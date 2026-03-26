3170473a55412ed43cd63ce6fc3446be80b37f3c
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-03-26 04:51:30 -0700

    implemented login route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
 
+// Route for Registration
 router.post('/register', authController.registerUser)
+
+// Route for Login
+router.post('/api/auth/login', authController.loginUser)
 
 module.exports = router
