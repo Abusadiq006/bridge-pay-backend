6a2139a402827bc49518c2a27f69b865cccee629
Farmer Abusadiq <Abusadiq006@gmail.com>
2026-04-09 04:34:09 -0700

    implementing cors methods

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ const app = express()
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173/'
+    origin: 'http://localhost:5173/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }))
 app.use(express.json())
 app.use('/api/user', userRoutes)
